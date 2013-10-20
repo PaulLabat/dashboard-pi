d3661d2c3acaceb3e8d83c5af25b710f6a319726
Paul Labat <labat.paul@gmail.com>
2013-10-20 20:42:54 +0200

    Modification du style pour les niveaux de crue

--- crueseine.py
+++ crueseine.py
@@ -50,8 +50,7 @@ def ecritCrueSeine(pageHtml):
 
 
 	pageHtml.write("""\n<div id="crue"> <h3> Info crues de la Seine</h3>\n""")
-	pageHtml.write("""<div id="alerte">Niveau de l'alerte : {}</div>\n""".format(niveauAlerte))
-
+	pageHtml.write("""<div id="alerte">Niveau de l'alerte : <div id="{}">{}</div></div>\n""".format(niveauAlerte, niveauAlerte))
 	pageHtml.write("""<div id="description"> {}</div>\n""".format(getDescription(niveauAlerte)))
 	pageHtml.write("""<div id="dateMaj"> Dernière mise à jour : {} </div>\n""".format(dateMaj))
 	pageHtml.write("</div>\n")#fin div crue
